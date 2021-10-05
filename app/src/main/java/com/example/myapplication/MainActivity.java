8704cd08c940788dd1eb9e33c39a1656154dad0b
0x337 <t5r@hotmail.fr>
2021-10-05 15:23:46 +0300

    Added button to change Your text

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,18 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textViewMain;
+    private TextView editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.textViewMain = findViewById(R.id.textViewMain);
+        this.editText = findViewById(R.id.editText);
     }
 
     public void onBtnClick(View view) {
@@ -25,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void onBtnClick2(View view) {
         textViewMain.setTextColor(Color.rgb(200,100,50));
+        editText.setTextColor(Color.rgb(0,200,50));
+
     }
     // this.TextColor.setTextColor("");
 }
