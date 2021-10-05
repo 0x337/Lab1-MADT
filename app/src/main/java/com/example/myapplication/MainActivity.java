a03cce8502079593b812abd0271979eae7cb4896
0x337 <t5r@hotmail.fr>
2021-10-05 13:47:32 +0300

    First commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,16 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.textViewMain = findViewById(R.id.textViewMain)
     }
 
     public void onBtnClick(View view) {
+        this.textViewMain.setText("Text Changed Successfully");
     }
 }
